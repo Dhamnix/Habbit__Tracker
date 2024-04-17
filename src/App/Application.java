fdf7fa1b61599e58c3b8796d044cce21759e8264
Habbit-Tracker <eh.vandaki@gmail.com>
2024-04-17 13:15:55 +0330

    start4 user monde

--- src/App/Application.java
+++ src/App/Application.java
@@ -3,8 +3,8 @@ package App;
 import UserPackage.User;
 
 public class Application {
-
     private User[] users = new User[100];
+    static int howMuch = 0;
     int j = 0 ;
     public User[] getUserByName(String name){
         User[] u = new User[5];
@@ -18,4 +18,16 @@ public class Application {
      if (j!=0)   return u;
      else return null;
     }
+
+    public void addUser(User u){
+        users[howMuch]=u;
+        howMuch++;
+    }
+    public void removeUser(User u){
+        for (int i =0 ; i!=howMuch; i++){
+            if (u == users[i])
+                users[i] = null;
+        }
+    }
 }
+
